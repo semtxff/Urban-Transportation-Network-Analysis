422aa30142c25cde8827c25654423d51d0061819
semtxff <137172083+semtxff@users.noreply.github.com>
2024-06-26 16:54:44 +0800

    Add files via upload

--- Urban Transportation Project/Data visualization.py
+++ Urban Transportation Project/Data visualization.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import folium
 
-# 加载数据
+# Load data
 data = pd.read_csv('urban_transport_network_stops.csv')
 
-# 创建地图对象
+# Create map Objects
 mymap = folium.Map(location=[data['latitude'].mean(), data['longitude'].mean()], zoom_start=12)
 
-# 添加站点到地图
+# Add stop to map
 for index, row in data.iterrows():
     folium.Marker(
         location=[row['latitude'], row['longitude']],
         popup=f"Stop ID: {row['stop_id']}<br>Name: {row['name']}<br>Zone Type: {row['zone_type']}"
     ).add_to(mymap)
 
-# 保存地图
+# Save map
 mymap.save('interactive_map.html')
