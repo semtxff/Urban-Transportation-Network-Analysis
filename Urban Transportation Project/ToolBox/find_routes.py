dce45de2b23f6bdbdd2f51a927a317e43248e4be
X623 <2022310893@bipt.edu.cn>
2024-07-01 16:37:31 +0800

    Add files via upload

--- Urban Transportation Project/ToolBox/find_routes.py
+++ Urban Transportation Project/ToolBox/find_routes.py
@@ -1,21 +1,4 @@
 import networkx as nx
-import os
-import sys
-
-
-def node_label():
-    node_labels = {
-        1: "Chatelet",
-        2: "Gare de Lyon",
-        3: "Bastille",
-        4: "Nation",
-        5: "Opera",
-        6: "Republique",
-        7: "Montparnasse",
-        8: "La Defense",
-        9: "Saint-Lazare"
-    }
-    return node_labels
 
 def find_all_paths(G, start_node, end_node):
     # 查找所有从start_node到end_node的路径
@@ -28,7 +11,7 @@ def print_paths(all_paths, node_labels):
     for i, path in enumerate(all_paths, 1):
         path_labels = [node_labels[node] for node in path]
         print(f"Path {i}: {' -> '.join(path_labels)}")
-        
+
 # 在其他代码中调用
 start_node = 1
 end_node = 3
