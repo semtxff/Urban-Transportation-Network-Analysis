73d0c5b36c38c8cf67cca0a32a1913c4e7f967fa
semtxff <137172083+semtxff@users.noreply.github.com>
2024-07-01 23:44:46 +0800

    Add files via upload

--- Urban Transportation Project/ToolBox/find_highest_centrality.py
+++ Urban Transportation Project/ToolBox/find_highest_centrality.py
@@ -12,35 +12,26 @@ from ToolBox.plt_graph import G
 from ToolBox.plt_graph import plot_transport_network
 
 def find_highest_centrality(graph):
-    max_out_degree = -1
-    max_in_degree = -1
-    max_out_node = None
-    max_in_node = None
+    max_total_degree = -1
+    max_total_node = None
     
     for node, attributes in graph.items():
-        if 'out_degree' in attributes and attributes['out_degree'] > max_out_degree:
-            max_out_degree = attributes['out_degree']
-            max_out_node = node
-        if 'in_degree' in attributes and attributes['in_degree'] > max_in_degree:
-            max_in_degree = attributes['in_degree']
-            max_in_node = node
+        total_degree = attributes['out_degree'] + attributes['in_degree']
+        if total_degree > max_total_degree:
+            max_total_degree = total_degree
+            max_total_node = node
     
-    return max_out_node, max_out_degree, max_in_node, max_in_degree
+    return max_total_node, max_total_degree
 
 
 # 找到中心度最高的车站
-max_out_node, max_out_degree, max_in_node, max_in_degree = find_highest_centrality(G)
+max_total_node, max_total_degree = find_highest_centrality(G)
 
 # 输出结果
-if max_out_node is not None:
-    print(f"The station with the highest out-degree (most outgoing connections) is {G[max_out_node]['name']} ({max_out_node}) with {max_out_degree} connections.")
+if max_total_node is not None:
+    print(f"The station with the highest total degree (sum of out-degree and in-degree) is {G[max_total_node]['name']} ({max_total_node}) with {max_total_degree} connections.")
 else:
-    print("No node with outgoing connections found.")
-
-if max_in_node is not None:
-    print(f"The station with the highest in-degree (most incoming connections) is {G[max_in_node]['name']} ({max_in_node}) with {max_in_degree} connections.")
-else:
-    print("No node with incoming connections found.")
+    print("No node with connections found.")
 
 # 绘制图形
 plot_transport_network(G)
