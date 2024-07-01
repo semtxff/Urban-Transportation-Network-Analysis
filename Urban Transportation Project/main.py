5a5357bd3faae8fefcea4cc6977b684d35fef75a
X623 <2022310893@bipt.edu.cn>
2024-07-01 19:19:37 +0800

    Add files via upload

--- Urban Transportation Project/main.py
+++ Urban Transportation Project/main.py
@@ -1,4 +1,4 @@
-def test_dev_function1():
+def directed_graph_function():
     from ToolBox.directed_graph import draw_graph, load_stops_df, load_routes_df, create_graph, node_label, find_most_central_node
     # 使用 draw_graph 函数
     stops_df = load_stops_df()
@@ -9,20 +9,21 @@ def test_dev_function1():
     print()
     find_most_central_node(G, labels)
 
-def test_dev_function2():
+def find_routes_function():
     from ToolBox.find_routes import print_paths, find_all_paths
     from ToolBox.directed_graph import load_routes_df, load_stops_df, node_label, create_graph
     stops_df = load_stops_df()
     routes_df = load_routes_df()
     G = create_graph(stops_df, routes_df)
-    start_node = 1
-    end_node = 3
+    print()
+    start_node = int(input("Please set start node:"))
+    end_node = int(input("Please set end node:"))
     all_paths = find_all_paths(G, start_node, end_node)
     node_labels = node_label()
     print()
     print_paths(all_paths, node_labels)
 
-def test_dev_function3():
+def route_effciency_function():
     from ToolBox.directed_graph import load_routes_df, load_stops_df
     from ToolBox.route_efficiency import shortest_path, create_and_configure_graph
     stops_df = load_stops_df()
@@ -33,9 +34,9 @@ def test_dev_function3():
 
 # 运行测试的函数
 def run_tests():
-    test_dev_function1()
-    test_dev_function2()
-    test_dev_function3()
+    directed_graph_function()
+    find_routes_function()
+    route_effciency_function()
     print()
     print("All tests passed!")
     print()
