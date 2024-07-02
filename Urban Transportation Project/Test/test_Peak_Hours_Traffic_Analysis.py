4bea0f5c9eb11d8570dc96571eee73ff0af11e9f
youknowss114514 <2532835699@qq.com>
2024-07-03 00:33:11 +0800

    Add files via upload

--- Urban Transportation Project/Test/test_Peak_Hours_Traffic_Analysis.py
+++ Urban Transportation Project/Test/test_Peak_Hours_Traffic_Analysis.py
@@ -1,6 +1,5 @@
 import unittest
 import pandas as pd
-from io import StringIO
 import sys
 import os
 
@@ -8,30 +7,54 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(current_dir)
 sys.path.append(project_root)
 
-from ToolBox.Peak_Hours_Traffic_Analysis import peak_hour_traffic_analysis, optimize_peak_hour_routes
+from ToolBox.Peak_Hours_Traffic_Analysis import create_graph, analyze_peak_hours_traffic, find_all_paths, calculate_path_time
 
 class test_Peak_Hours_Traffic_Analysis(unittest.TestCase):
-
     def setUp(self):
-        stops_data = """stop_id,stop_name
-                        1,Stop A
-                        2,Stop B"""
-        routes_data = """route_id,distance
-                         1,10
-                         2,20"""
-        self.stops_df = pd.read_csv(StringIO(stops_data))
-        self.routes_df = pd.read_csv(StringIO(routes_data))
+        self.stops_df = pd.DataFrame({
+            'stop_id': [1, 2, 3],
+            'name': ['Stop A', 'Stop B', 'Stop C'],
+            'longitude': [0.0, 1.0, 2.0],
+            'latitude': [0.0, 1.0, 2.0]
+        })
+        
+        self.routes_df = pd.DataFrame({
+            'start_stop_id': [1, 2, 3],
+            'end_stop_id': [2, 3, 1],
+            'distance': [10.0, 15.0, 20.0]
+        })
 
-    def test_peak_hour_traffic_analysis(self):
-        expected_congestion_factor = 1.5
-        expected_peak_hour_distance = self.routes_df['distance'] * expected_congestion_factor
-        result_df = peak_hour_traffic_analysis(self.routes_df.copy())
+        self.graph = create_graph(self.stops_df, self.routes_df)
+        self.optimized_routes_df = analyze_peak_hours_traffic(self.routes_df, congestion_factor=1.5, stop_delay=2)
 
-        pd.testing.assert_series_equal(result_df['peak_hour_distance'], expected_peak_hour_distance, check_names=False)
+    def test_create_graph(self):
+        expected_graph = {
+            1: {'pos': (0.0, 0.0), 'name': 'Stop A', 'out_degree': 1, 'in_degree': 1, 2: 10.0},
+            2: {'pos': (1.0, 1.0), 'name': 'Stop B', 'out_degree': 1, 'in_degree': 1, 3: 15.0},
+            3: {'pos': (2.0, 2.0), 'name': 'Stop C', 'out_degree': 1, 'in_degree': 1, 1: 20.0}
+        }
+        self.assertEqual(self.graph, expected_graph)
 
-    def test_optimize_peak_hour_routes(self):
-        optimized_df = optimize_peak_hour_routes(self.routes_df.copy())
-        pd.testing.assert_frame_equal(optimized_df, self.routes_df)
+    def test_analyze_peak_hours_traffic(self):
+        expected_optimized_routes_df = pd.DataFrame({
+            'start_stop_id': [1, 2, 3],
+            'end_stop_id': [2, 3, 1],
+            'distance': [10.0, 15.0, 20.0],
+            'peak_hour_distance': [15.0, 22.5, 30.0],
+            'peak_hour_time': [3.5, 4.25, 5.0]
+        })
+        pd.testing.assert_frame_equal(self.optimized_routes_df, expected_optimized_routes_df)
+
+    def test_find_all_paths(self):
+        expected_paths = [[1, 2, 3]]
+        all_paths = find_all_paths(self.graph, 1, 3)
+        self.assertEqual(all_paths, expected_paths)
+
+    def test_calculate_path_time(self):
+        path = [1, 2, 3]
+        expected_time = 3.5 + 4.25
+        total_time = calculate_path_time(self.graph, path, self.optimized_routes_df)
+        self.assertEqual(total_time, expected_time)
 
 if __name__ == '__main__':
     unittest.main()
